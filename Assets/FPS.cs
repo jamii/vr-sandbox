3e9d8d416a2c892cf570ae35c1b646e2e4196d5b
Jamie Brandon <jamie@scattered-thoughts.net>
2016-07-18 17:24:44 +0200

    Count dropped frames

--- Assets/FPS.cs
+++ Assets/FPS.cs
@@ -3,26 +3,31 @@ using System.Collections;
 
 public class FPS : MonoBehaviour
 {
-    float deltaTime = 0.0f;
+    public int missedFrames = 0;
+    private GUIStyle style;
+    private Rect rect;
+
+    void Start()
+    {
+        int w = Screen.width, h = Screen.height;
+        rect = new Rect(0, 0, w, h * 2 / 100);
+        style = new GUIStyle();
+        style.alignment = TextAnchor.UpperLeft;
+        style.fontSize = h * 2 / 100;
+        style.normal.textColor = new Color(0.0f, 0.0f, 0.5f, 1.0f);
+    }
 
     void Update()
     {
-        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+        if (Time.deltaTime > 0.012)
+        {
+            missedFrames += 1;
+        }
     }
 
     void OnGUI()
     {
-        int w = Screen.width, h = Screen.height;
-
-        GUIStyle style = new GUIStyle();
-
-        Rect rect = new Rect(0, 0, w, h * 2 / 100);
-        style.alignment = TextAnchor.UpperLeft;
-        style.fontSize = h * 2 / 100;
-        style.normal.textColor = new Color(0.0f, 0.0f, 0.5f, 1.0f);
-        float msec = deltaTime * 1000.0f;
-        float fps = 1.0f / deltaTime;
-        string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+        string text = string.Format("{0:0.0} missed frames)", missedFrames);
         GUI.Label(rect, text, style);
     }
 }
